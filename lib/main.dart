6256fe736b121a1da8939a203b4891246f62ff7e
nkun0003 <nkun0003@algonquinlive.com>
2024-11-19 02:52:28 -0500

    fixed some spacing issues and styling

--- lib/main.dart
+++ lib/main.dart
@@ -13,130 +13,144 @@ class MainApp extends StatelessWidget {
       title: 'Images and Assets',
       theme: ThemeData(primarySwatch: Colors.brown, fontFamily: 'Inter'),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Images and Assets',
-            style: TextStyle(color: Colors.white),
+          appBar: AppBar(
+            title: const Text(
+              'Images and Assets',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.brown,
           ),
-          backgroundColor: Colors.brown,
-        ),
-        body: ListView(
-          children: [
-            // Top Horizontal ListView used internet images
-            SizedBox(
-              height: 350,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800',
-                      width: 400,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 8.0), // here used global padding
+            child: ListView(
+              children: [
+                // Top Horizontal ListView used internet images
+                SizedBox(
+                  height: 350,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.only(top: 20),
+                    children: [
+                      Image.network(
+                        'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800',
+                        width: 400,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 10), // Space between images
+                      Image.network(
+                        'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800',
+                        width: 400,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 10), // Space between images
+                      Image.network(
+                        'https://images.unsplash.com/photo-1547721064-da6cfb341d50?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800',
+                        width: 400,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800',
-                      width: 400,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.network(
-                      'https://images.unsplash.com/photo-1547721064-da6cfb341d50?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800',
-                      width: 400,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20), // here spacing between sections
+                ),
+                const SizedBox(height: 10), // here spacing between sections
 
-            // my listTiles of the Middle
-            const ListTile(
-              leading: Icon(Icons.map),
-              title: Text(
-                'Inter 100',
-                style: TextStyle(fontWeight: FontWeight.w100, fontSize: 30),
-              ),
-            ),
-            const ListTile(
-              leading: Icon(Icons.star_half),
-              title: Text(
-                'Inter 400',
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 30),
-              ),
-            ),
-            const ListTile(
-              leading: Icon(Icons.traffic),
-              title: Text(
-                'Inter 700',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30),
-              ),
-            ),
-            const ListTile(
-              leading: CircleAvatar(child: Text('R1')),
-              title: Text(
-                'Doto',
-                style: TextStyle(fontFamily: 'Doto', fontSize: 30),
-              ),
-            ),
-            const ListTile(
-              leading: CircleAvatar(child: Text('R1')),
-              title: Text(
-                'Doto',
-                style: TextStyle(fontFamily: 'Doto', fontSize: 30),
-              ),
-            ),
-            const SizedBox(height: 20), // here spacing between sections
+                // my listTiles of the Middle
+                Container(
+                  color: const Color.fromARGB(255, 219, 196, 188),
+                  child: const ListTile(
+                    leading: Icon(Icons.map),
+                    title: Text(
+                      'Inter 100',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w100, fontSize: 30),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
 
-            // Bottom Horizontal ListView where i used local Assets i downloaded Images)
-            SizedBox(
-              height: 350,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      'images/pic1.jpg',
-                      width: 400,
-                      height: 200,
-                      fit: BoxFit.cover,
+                Container(
+                  color: const Color.fromARGB(255, 219, 196, 188),
+                  child: const ListTile(
+                    leading: Icon(Icons.star_half),
+                    title: Text(
+                      'Inter 400',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w400, fontSize: 30),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      'images/pic2.jpg',
-                      width: 400,
-                      height: 200,
-                      fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 10),
+
+                Container(
+                  color: const Color.fromARGB(255, 219, 196, 188),
+                  child: const ListTile(
+                    leading: Icon(Icons.traffic),
+                    title: Text(
+                      'Inter 700',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 30),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      'images/pic3.jpg',
-                      width: 400,
-                      height: 200,
-                      fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  color: const Color.fromARGB(255, 219, 196, 188),
+                  child: const ListTile(
+                    leading: CircleAvatar(child: Text('R1')),
+                    title: Text(
+                      'Doto',
+                      style: TextStyle(fontFamily: 'Doto', fontSize: 30),
                     ),
                   ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  color: const Color.fromARGB(255, 219, 196, 188),
+                  child: const ListTile(
+                    leading: CircleAvatar(child: Text('R1')),
+                    title: Text(
+                      'Doto',
+                      style: TextStyle(fontFamily: 'Doto', fontSize: 30),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10), // here spacing between sections
+
+                // Bottom Horizontal ListView where I used local Assets (Downloaded Images)
+                SizedBox(
+                  height: 350,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Image.asset(
+                        'images/pic1.jpg',
+                        width: 400,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 10), // Space between images
+                      Image.asset(
+                        'images/pic2.jpg',
+                        width: 400,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 10), // Space between images
+                      Image.asset(
+                        'images/pic3.jpg',
+                        width: 400,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
